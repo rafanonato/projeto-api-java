882e9171746f89eac6a27ec4757fd8f1a043f9d7
rafanonato <rf.nonato@gmail.com>
2017-09-16 19:54:03 -0300

    Iniciando Api Java

---
+++ src/main/java/com/api/java/projetoapijava/ProjetoApiJavaApplication.java
@@ -0,0 +1,12 @@
+package com.api.java.projetoapijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiJavaApplication.class, args);
+	}
+}
